adc9c0e22e17a56488301c1cafba39997b71b806
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-06-02 09:38:58 -0700

    Remove name check

--- commands/setlog.js
+++ commands/setlog.js
@@ -13,8 +13,8 @@ module.exports = {
      */
 
     execute: async function (message, args, client) {
-        let channel = message.guild.channels.cache.find(c => c.name.toLowerCase().includes(args[0].toLowerCase())) || message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel
-        client.db.guild.set(`logs_${message.guild.id}`)
+        let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel
+        client.db.guild.set(`logs_${message.guild.id}`, channel.id)
         return message.channel.send(`Set logs channel as ${channel.toString()}`)
     }
 }
