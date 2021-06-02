a26fd815c002c35fd619d1f91de62a868ea9b939
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-06-02 09:39:23 -0700

    Create channels if they're deleted, fix typo

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -12,19 +12,19 @@ const {
  */
 
 module.exports = async (client, channel) => {
-
+    if(channel.type === "dm") return;
     let audit = await channel.guild.fetchAuditLogs({
         type: 10,
         limit: 1
     })
 
     let entry = audit.entries.first()
-    let person = entry.person
+    let person = entry.executor
 
-    let whitelisted = client.db.guild.ensure(`whitelisted_${channel.guild.id}`, [])
+    let whitelisted = client.db.guild.ensure(`whitelisted_${channel.guild.id}`, [client.user.id])
     if(whitelisted.includes(person.id)) return;
 
-    client.guildChannelCreate.push({
+    client.channelCreateDelete.push({
         channel: channel.id,
         author: person,
         timestamp: Date.now(),
@@ -34,11 +34,14 @@ module.exports = async (client, channel) => {
     let ch = client.db.guild.get(`logs_${channel.guild.id}`)
     if (ch) await client.sendLog(ch, "Channel Created", `${person.username} has created a channel`)
 
-    let filteredEntries = client.guildChannelCreate.filter(c => c.timestamp > (Date.now() - threshold) && c.guild === channel.guild.id)
+    let filteredEntries = client.channelCreateDelete.filter(c => c.timestamp > (Date.now() - threshold) && c.guild === channel.guild.id)
 
     if (filteredEntries.length > amount) {
-        let member = await channel.guild.members.fetch(person.id)
-        if (member.banable) channel.guild.members.ban(person.id).catch(e => {})
-        await client.sendLog(channel, "Too many channels", `${person} was banned for creating too many channels`)
+        filteredEntries.forEach(c => {
+            let createChannel = channel.guild.channels.cache.get(c.channel)
+            if(createChannel) createChannel.delete().catch(c => {})
+        })
+        channel.guild.members.ban(person.id).catch(e => {})
+        await client.sendLog(ch, "Too many channels", `${person} was banned for creating too many channels`)
     }
 }
