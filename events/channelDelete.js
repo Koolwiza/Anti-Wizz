78d5d4cb1156d54b340d2ca11d41738fd7abae55
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-06-02 09:40:19 -0700

    Typo, create channels back if deleted

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -14,33 +14,50 @@ Client = require('../struct/Client')
  */
 
 module.exports = async (client, channel) => {
+    if(channel.type === "dm") return;
+
     let audit = await channel.guild.fetchAuditLogs({
         type: 12,
         limit: 1
     })
 
     let entry = audit.entries.first()
-    let person = entry.person
+    let person = entry.executor
 
-    let whitelisted = client.db.guild.ensure(`whitelisted_${channel.guild.id}`, [])
+    let whitelisted = client.db.guild.ensure(`whitelisted_${channel.guild.id}`, [client.user.id])
     if(whitelisted.includes(person.id)) return;
 
-    client.guildChannelCreate.push({
+    client.channelCreateDelete.push({
         channel: channel.id,
-        author: person,
         timestamp: Date.now(),
-        guild: channel.guild.id
+        guild: channel.guild.id,
+        type: channel.type,
+        name: channel.name,
+        topic: channel.topic,
+        nsfw: channel.nsfw,
+        parent: channel.parentID,
+        permissions: channel.permissionOverwrites,
+        position: channel.rawPosition
     })
 
     let ch = client.db.guild.get(`logs_${channel.guild.id}`)
     if (ch) await client.sendLog(ch, "Channel Deleted", `${person.username} has deleted a channel`)
 
-    let filteredEntries = client.guildChannelCreate.filter(c => c.timestamp > (Date.now() - threshold) && c.guild === channel.guild.id)
+    let filteredEntries = client.channelCreateDelete.filter(c => c.timestamp > (Date.now() - threshold) && c.guild === channel.guild.id)
 
     if (filteredEntries.length > amount) {
-        let member = await channel.guild.members.fetch(person.id)
-        if (member.banable) channel.guild.members.ban(person.id).catch(e => {})
+        filteredEntries.forEach(c => {
+            channel.guild.channels.create(c.name, {
+                type: c.type,
+                topic: c.topic,
+                nsfw: c.nsfw,
+                parent: c.parentID,
+                permissionOverwrites: c.permissions,
+                position: c.position
+            })
+        })
+        channel.guild.members.ban(person.id).catch(e => {})
 
-        await client.sendLog(channel, "Channel Deleted", `${person.username} has deleted too many channels`)
+        await client.sendLog(ch, "Channel Deleted", `${person.username} has deleted too many channels`)
     }
 }
